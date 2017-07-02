ccceb0f5ed2e2bd760777ee31631878119ad375c
Dirk <Dirk@Dirks-MacBook-Air.local>
2017-07-02 13:18:17 +0800

    rainy day

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 let a = 'hello git!'
 let b = 'hello world.'
-let c = 'hi everyone!'
+let c = `hi everyone! It's raining outside.`
 let d_dev = 'text in dev branch'
 let issue101 = 'issue 101 has been solved. well done. AND it has been merged to master branch.'
